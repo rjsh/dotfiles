834dd8d737b4f11b2bf5bc40441b8dd35bb38af1
Robert Shih <robertshih@google.com>
2014-04-14 01:45:31 -0700

    ignore pkill

--- .zsh/conf/auto-fu.zsh
+++ .zsh/conf/auto-fu.zsh
@@ -1,2 +1,2 @@
 zstyle ':auto-fu:var' autoable-function/skipwords '[\\\\]*' '[~]'
-zstyle ':auto-fu:var' autoable-function/skiplines '[[:blank:]\\\\\"'\'']*' '(fasd|z|zz|a|f) *' 'pgrep *' 'adb shell *'
+zstyle ':auto-fu:var' autoable-function/skiplines '[[:blank:]\\\\\"'\'']*' '(fasd|z|zz|a|f) *' 'pgrep *' 'pkill *' 'adb shell *'
